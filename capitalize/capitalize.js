d3b29dc8527bd1f8bc2732a771776c4066e0fc47
Emmanuel Danison <117293986+LazyEllis@users.noreply.github.com>
2024-05-05 22:38:30 +0100

    feat: Implement capitalize function

---
+++ capitalize/capitalize.js
@@ -0,0 +1,9 @@
+const capitalize = (string) =>
+  !string
+    ? string
+    : string
+        .split(" ")
+        .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+        .join(" ");
+
+export default capitalize;
